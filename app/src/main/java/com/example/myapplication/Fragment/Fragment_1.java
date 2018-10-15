a290818475297e1699fee5df111aa9facddc7260
AhmedRammadan <43143580+AhmedRammadan@users.noreply.github.com>
2018-10-16 01:31:46 +0200

    version 4

--- app/src/main/java/com/example/myapplication/Fragment/Fragment_1.java
+++ app/src/main/java/com/example/myapplication/Fragment/Fragment_1.java
@@ -64,47 +64,7 @@ public class Fragment_1 extends Fragment {
         return view;
     }
     public void  checkPermission(){
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
-           return;
-        }
-        else{
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) getContext(),Manifest.permission.CALL_PHONE)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                Toast.makeText(getContext(), "done", Toast.LENGTH_SHORT).show();
-                return;
-            }  // No explanation needed; request the permission
-            int MY_PERMISSIONS_REQUEST_READ_CONTACTS =1;
-            ActivityCompat.requestPermissions((Activity) getContext(),new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
 
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
-            return;
-        }
     }
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
 
-            // other 'case' lines to check for other
-            // permissions this app might request.
-        }
-    }
 }
